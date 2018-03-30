8b2d3ef74f0d9f45b09159702784a72f10f79697
Chandrahasa Reddy <chandrahasa.reddy@rbc.com>
2018-03-30 14:49:09 -0400

    Recipe assignement

--- src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -1,18 +1,18 @@
 package guru.springframework.spring5recipeapp.domain;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
-public class UnitOfMeasure {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
 
-    //id value
     public Long getId() {
         return id;
     }
@@ -27,5 +27,13 @@ public class UnitOfMeasure {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
     }
 }
